d0076f66acd77b2f931008982ef80e60c378ca34
Timo Sarkar <71646577+timo-cmd2@users.noreply.github.com>
2021-06-12 10:33:34 +0200

    Create body.cpp

---
+++ body/body.cpp
@@ -0,0 +1,15 @@
+/** 
+ * This is the main trojan body xD
+ */
+#include <stdio.h>
+#include <windows.h>
+#include <stdlib.h>
+#include <string.h>
+#include <winsock.h>
+#include <tlhelp32.h>
+#include <comutil.h>
+
+// windooze EntryPoint 
+int WinApi Winmain() {
+  return 0;
+}
