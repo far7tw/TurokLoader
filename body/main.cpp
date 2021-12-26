49470824c660583ed80c38802a427055364cd7de
Timo Sarkar <71646577+APT777@users.noreply.github.com>
2021-12-26 15:44:10 +0100

    Adding main entrypoint for the body

---
+++ body/main.cpp
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <windows.h>
+#include <string.h>
+
+namespace Body
+{ 
+  class EntryPoint 
+  {
+    private static Random rand = new Random(Environment.TickCount);
+    private static string chars = "각갂갃간갅갆갇갈갉갊갋갌갍갎갏감갑값갓갔강갖갗갘같갚갛개객갞갟갠갡갢갣갤갥갦갧갨갩갪갫갬갭갮갯";
+    
+    public void Main(string[] args)
+    {
+      
+    }
+  }
+}
